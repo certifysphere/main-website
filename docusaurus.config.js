789087ad71ea6603f34948839eda9e1f4cd6ee41
CertifySphere <132380723+certifysphere@users.noreply.github.com>
2023-08-27 16:39:13 -0400

    announcement bar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -79,9 +79,17 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      metadata: [{ name: 'keywords', content: 'free coding bootcamp, learn python, learn react, aws, software consulting' }],
+      metadata: [{ name: 'keywords', content: 'free coding bootcamp, learn python, learn react, aws, mongodb, postgres, software consulting' }],
       // Replace with your project's social card
       image: 'img/certifysphere_banner_social.png',
+      announcementBar: {
+        id: 'support_us',
+        content:
+        'Registration is open for the fall free internship and bootcamp. Please register <a target="_blank" rel="noopener noreferrer" href="https://forms.gle/Lp6RQ1unCzZB5Pyn9">here</a>.',
+        backgroundColor: '#fafbfc',
+        textColor: '#091E42',
+        isCloseable: true,
+      },
       navbar: {
         title: 'CertifySphere',
         logo: {
