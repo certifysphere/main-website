dca115b79a5cc4f6e8621ee9313342e5653efbce
certifysphere <certifysphere.consulting@gmail.com>
2025-02-08 19:53:08 -0600

    githubpage workflow

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'CertifySphere',
-  tagline: 'IT Consulting & Training Solutions',
+  tagline: 'AI, Cloud Trainings, Consulting, and Software Development',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
